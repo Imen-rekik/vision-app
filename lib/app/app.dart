21a4b9b5082676f092630a439818e2c8628b8164
unknown <imen.rekik2023@gmail.com>
2026-08-21 02:51:53 +0100

    removed test.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../screens/home/home_screen.dart';
-import '../screens/live_test/live_test_screen.dart';
 import '../screens/onboarding/permissions_screen.dart';
 import '../screens/onboarding/voice_onboarding_screen.dart';
 import '../screens/splash/splash_screen.dart';
@@ -21,7 +20,6 @@ class VisionApp extends StatelessWidget {
         '/permissions': (_) => const PermissionsScreen(),
         '/voice-onboarding': (_) => const VoiceOnboardingScreen(),
         '/home': (_) => const HomeScreen(),
-        '/live-test': (_) => const LiveTestScreen(),
       },
     );
   }
